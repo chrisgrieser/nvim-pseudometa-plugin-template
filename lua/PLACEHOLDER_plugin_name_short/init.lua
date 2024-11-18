bfd8bf0e232c4bf51717edadb821036a353d7202
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-11-18 15:49:31 +0100

    Update init.lua

--- lua/PLACEHOLDER_plugin_name_short/init.lua
+++ lua/PLACEHOLDER_plugin_name_short/init.lua
@@ -1,7 +1,7 @@
 local M = {}
 --------------------------------------------------------------------------------
 
----@param userConfig? PLACEHOLDER_plugin_name_short.config
+---@param userConfig? PLACEHOLDER_plugin_name_short_capitalized.config
 M.setup = function(userConfig) require("PLACEHOLDER_plugin_name_short.config").setup(userConfig) end
 
 --------------------------------------------------------------------------------
